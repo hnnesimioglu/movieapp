edf6e0f6a4331574e6fa91af6f93894e9821c53a
Nesimi <hasanesimi01@gmail.com>
2023-04-07 11:06:06 +0300

    69. auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,15 @@ import { MovieDetailsComponent } from "./movie-details/movie-details.component";
 import { MovieCreateComponent } from "./movie-create/movie-create.component";
 import { CategoryCreateComponent } from './category-create/category-create.component';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  { path: 'movies', component: MoviesComponent },
+  { path: 'movies', component: MoviesComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: 'movies', pathMatch: 'full' },
-  { path: 'movies/category/:categoryId', component: MoviesComponent },
-  { path: 'movies/create', component: MovieCreateComponent },
-  { path: 'categories/create', component: CategoryCreateComponent },
-  { path: 'movies/:movieId', component: MovieDetailsComponent },
+  { path: 'movies/category/:categoryId', component: MoviesComponent, canActivate: [AuthGuard] },
+  { path: 'movies/create', component: MovieCreateComponent, canActivate: [AuthGuard] },
+  { path: 'categories/create', component: CategoryCreateComponent, canActivate: [AuthGuard] },
+  { path: 'movies/:movieId', component: MovieDetailsComponent, canActivate: [AuthGuard] },
   { path: 'auth', component: AuthComponent },
 ];
 
